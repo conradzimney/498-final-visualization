9fe9898e315c69f6e28b17201c09c82bbe28fcb9
conradzimney <conradzimney@gmail.com>
2015-05-31 16:59:28 -0700

    Centered. svg in right place. Let's go.

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 
-var width = window.innerWidth;
-var height = window.innerHeight;
+var width = 800; 
+var height = 300; 
 
 var svg = d3.select("main").append("svg")
+    .attr("class", "guy")
     .attr("width", width)
     .attr("height", height);
 
 var rect = svg.append("rect")
     .attr("class", "rect")
-    .attr("width", 400)
-    .attr("height", 600)
-    .attr("transform", "translate(300)");
+    .attr("width", width)
+    .attr("height", height);
 
-drawSource(80, 0, 100);
-drawSource(100, 0, 300);
-drawSource(150, 0, 500);
+//drawSource(300, 0, 100);
+//drawSource(400, 0, 300);
+drawSource(600, 100, 150);
 
 function drawSource(amp, x, y) {
     var circ = svg.append("circle")
         .attr("class", "dot")
         .attr("r", 8)
-        .attr("cy", 610 - y)
-        .attr("cx", width / 2 + x);
+        .attr("cy", y)
+        .attr("cx", x);
 
     setInterval(function() {
       svg.append("circle")
           .attr("class", "ring")
-          .attr("cy", 610 - y)
-          .attr("cx", width / 2 + x)
+          .attr("cy", y)
+          .attr("cx", x)
           .attr("r", 6)
           .style("stroke-width", 3)
           .style("stroke", "gray")
         .transition()
           .ease("linear")
-          .duration(1000)
+          .duration(2000)
           .style("stroke-opacity", .7)
           .style("stroke-width", 1)
           .style("stroke", "gray")
-          .attr("r", 200)
+          .attr("r", 700)
           .remove();
     }, amp);
 }
