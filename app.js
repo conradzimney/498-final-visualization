614433b24258da638873d9b1689504769864db17
conradzimney <conradzimney@gmail.com>
2015-05-31 22:43:10 -0700

    need to figure out reflections

--- app.js
+++ app.js
@@ -7,28 +7,23 @@ var svg = d3.select("#viz").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-var rect = svg.append("rect")
+// TO DO STILL (5/31/15 6:30 pm) :
+// 
+// Make drawsource function stop its original drawing when slider happens !!!!
+// Scientific encoding of amplitude and frequency to our viz (is it correct?) Yes it is.
+// 
+
+var amplitude = 5;
+var frequency = 600;
+var myVar;
+
+drawSource(frequency, amplitude, 100, 150, false);
+
+function drawSource(freq, amp, x, y, isRef) {
+    var rect = svg.append("rect")
     .attr("class", "rect")
     .attr("width", width)
     .attr("height", height);
-
-//drawSource(300, 0, 100);
-//drawSource(400, 0, 300);
-
-// TO DO STILL (5/31/15 6:30 pm) :
-// 
-// Make drawsource function stop its original drawing when slider happens
-// Scientific encoding of amplitude and frequency to our viz (is it correct?)
-// Make slider single and not ranged
-// Reflections (establish dissapation function)
-// 
-
-var amplitude = 10;
-var frequency = 400;
-
-drawSource(frequency, amplitude, 100, 150);
-
-function drawSource(freq, amp, x, y, isRef) {
   if (!isRef) {
     var circ = svg.append("circle")
         .attr("class", "dot")
@@ -37,7 +32,7 @@ function drawSource(freq, amp, x, y, isRef) {
         .attr("cx", x);
   }
 
-    setInterval(function() {
+   myVar = setInterval(function() {
       svg.append("circle")
           .attr("class", "ring")
           .attr("cy", y)
@@ -56,41 +51,55 @@ function drawSource(freq, amp, x, y, isRef) {
     }, freq);
 }
 
-function drawReflection(freq, amp, x, y) {
-  drawSource(freq, .3*amp, x+700, y, true);
-}
-
-drawReflection(frequency, amplitude, 100, 150);
-
+//drawReflections(frequency, amplitude, 100, 150);
 
 $(function() {
   $("#amp-slider").slider({
-    range: true,
     min: 1,
     max: 10,
-    value: 50,
+    value: 5,
     slide: function(event, ui) {
       $("#ampamount").val(ui.value);
+      clearInterval(myVar);
       amplitude = ui.value;
       drawSource(frequency, ui.value, 100, 150);
+      //drawReflections(frequency, ui.value, 100, 150);
     }
   });
-
+  $("#ampamount").val(amplitude);
 });
 
 $(function() {
   $("#freq-slider").slider({
-    range: true,
     min: 20,
     max: 2000,
-    value: 50,
+    value: 1000,
     slide: function(event, ui) {
       $("#freqamount").val(ui.value);
+      clearInterval(myVar);
       frequency = ui.value;
       drawSource(ui.value, amplitude, 100, 150);
+      //drawReflections(ui.value, amplitude, 100, 150);
     }
   });
+  $("#freqamount").val(frequency);
 });
+
+function drawReflections(freq, amp, x, y) {
+    setTimeout(function() {drawSource(freq, .3*amp, x, y - 150, true); }, 400);
+    setTimeout(function() {drawSource(freq, .3*amp, x, y + 150, true); }, 400);
+    setTimeout(function() {drawSource(freq, .3*amp, x - 100, y, true); }, 400);
+    setTimeout(function() {drawSource(freq, .3*amp, x + 700, y, true); }, 1700);
+    setTimeout(function() {drawSource(freq, .3*amp, x - 100, y - 150, true); }, 500);
+    setTimeout(function() {drawSource(freq, .3*amp, x - 100, y + 150, true); }, 500);
+}
+
+
+
+
+
+
+
 
 
 
